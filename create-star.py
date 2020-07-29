6254d7fdc3b5f0b0a8c0a3a9dacae364c7790d9d
shanthanaroja <62417823+shanthanaroja@users.noreply.github.com>
2020-07-29 23:09:03 +0530

    Create create-star.py

---
+++ create-star.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.color("blue","red")
+turtle.begin_fill()
+turtle.speed(10)
+for i in range(50):
+    turtle.forward(300)
+    turtle.left(170)
+turtle.end_fill()
+turtle.done()
